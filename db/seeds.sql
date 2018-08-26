28a15a750f8054ac157a8a99d27a235f7462a8bf
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-08-26 01:38:34 -0700

    Add file directory for application.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Sample db entries. --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
